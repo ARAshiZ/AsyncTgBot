1df92bfe5389c959b5e766f720eba7ed227ae638
Danya <d.yakowkin@yandex.ru>
2025-01-16 17:12:13 +0500

    create help command

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,12 +3,18 @@ import asyncio
 import logging
 import sys
 from aiogram import Dispatcher, Bot
+from aiogram.types import BotCommand
 from commands import register_user_commands
+from commands.constants import bot_commands
 
 async def main() -> None:
+    commands_for_bot = []
+    for cmd in bot_commands:
+        commands_for_bot.append(BotCommand(command=cmd[0], description=cmd[1]))
     dp = Dispatcher()
     bot = Bot(token=os.getenv('token'))
 
+    await bot.set_my_commands(commands=commands_for_bot)
     register_user_commands(dp)
     await dp.start_polling(bot)
 
--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -1,10 +1,12 @@
-__all__ = ['start']
+__all__ = ['register_user_commands']
 
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 
 from bot.commands.start import start_command
+from bot.commands.help import help_command
 
 
 def register_user_commands(router: Router) -> None:
-    router.message.register(start_command, CommandStart)
+    router.message.register(start_command, CommandStart())
+    router.message.register(help_command, Command(commands=['help']))
