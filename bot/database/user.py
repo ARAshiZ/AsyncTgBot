e3dd59b2105327b55ac45f4f1040be401b6e1a1a
Danya <d.yakowkin@yandex.ru>
2025-04-16 17:56:10 +0500

    create db foundament

---
+++ bot/database/user.py
@@ -0,0 +1,15 @@
+import datetime
+
+from sqlalchemy import Column, VARCHAR, INTEGER, DATE
+from .base import BaseModel
+
+class User(BaseModel):
+    __tablename__ = 'users'
+    
+    user_id = Column(INTEGER, primary_key=True, unique=True, nullable=False)
+    user_name = Column(VARCHAR(20), unique=False, nullable=True)
+    reg_date = Column(DATE, default=datetime.date.today())
+    update_date = Column(DATE, default=datetime.date.today())
+    
+    def __str__(self) -> str:
+        return f'user: {self.user_id}'
