c5884d89d6132694710f13610a99b603e29afa7b
Danya <d.yakowkin@yandex.ru>
2025-05-05 13:58:17 +0500

    create test_common (oredering command)

--- bot/tests/test_handlers/test_common.py
+++ bot/tests/test_handlers/test_common.py
@@ -1,10 +1,41 @@
 import pytest
 from unittest.mock import AsyncMock
+
+import pytest_asyncio
 from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.base import StorageKey
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import ReplyKeyboardRemove
+
 from bot.commands.common import cmd_order
+from bot.tests.mocked_bot import MockedBot
+from bot.tests.utils import TEST_USER, TEST_USER_CHAT
+
+@pytest_asyncio.fixture(scope='session')
+async def storage():
+    tmp_storage = MemoryStorage()
+    try:
+        yield tmp_storage
+    finally:
+        await tmp_storage.close()
+
+@pytest.fixture
+def bot():
+    return MockedBot()
 
 @pytest.mark.asyncio
-async def test_common_order_handler():
+async def test_common_order_handler(storage, bot):
     message = AsyncMock()
-    state = FSMContext()
+    state = FSMContext(
+        storage=storage,
+        key=StorageKey(bot_id=bot.id, user_id=TEST_USER.id, chat_id=TEST_USER_CHAT.id)
+    )
     await cmd_order(message=message, state=state)
+
+    assert await state.get_state() is None
+
+    message.answer.assert_called_with(
+        text="Выберите, что хотите заказать: "
+             "блюда (/food) или напитки (/drinks)",
+        reply_markup=ReplyKeyboardRemove()
+    )
