29a3de9135432dd2e086473095947fa48783a453
Danya <d.yakowkin@yandex.ru>
2025-05-05 15:43:09 +0500

    some fixes

--- bot/commands/keyboards.py
+++ bot/commands/keyboards.py
@@ -1,28 +1,33 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from typing import List
 
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 
 def make_row_keyboard(items: List[str]) -> ReplyKeyboardMarkup:
     row = [KeyboardButton(text=item) for item in items]
     return ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
 
-keyboard_builder= ReplyKeyboardBuilder()
+keyboard_builder= InlineKeyboardBuilder()
 
 
 keyboard_builder.button(
     text='Помощь',
     callback_data='help',
 )
-
-keyboard_builder.add(
-KeyboardButton(
+keyboard_builder.button(
     text='Стикер',
     callback_data='sticker',
-))
-keyboard_builder.add(
-    KeyboardButton(
+)
+keyboard_builder.button(
     text='В меню',
     callback_data='back',
-))
+)
+keyboard_builder.button(
+    text='keyb1',
+    callback_data='create_reaply1'
+)
+keyboard_builder.button(
+    text='keyb2',
+    callback_data='create_reaply2'
+)
